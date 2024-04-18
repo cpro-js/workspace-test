3d5da6f3af00b0c2deb14716bd7e4fc081ba8679
Jan-André Zinser <zinserjan@gmail.com>
2024-04-18 18:17:38 +0200

    fix: test

--- packages/publish-test2/src/index.ts
+++
@@ -1,3 +0,0 @@
-export const helloWorld: string = "Hello world!!";
-
-export { render } from "./react";
--- packages/publish-test2/src/react.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-import { MyProps } from "./types";
-
-export const render: FC<MyProps> = () => {
-  return <div>Test3</div>;
-};
